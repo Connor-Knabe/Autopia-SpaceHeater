c56d92a75e7211069890ba6bca42b4d3d3b10f16
Connor Knabe <connorknabe@gmail.com>
2016-08-05 15:25:16 -0500

    Fixing cron jobs

--- app/cronTimeService.js
+++ app/cronTimeService.js
@@ -1,5 +1,3 @@
-var CronJob = require('cron').CronJob;
-var cron = require('node-cron');
 
 var schedule = require('node-schedule');
 
@@ -7,30 +5,20 @@ var schedule = require('node-schedule');
 var timeHelper = require('./timeHelper');
 var _ = require('lodash');
 var minsBeforeFloat = 0;
-var cronJobs = [{
-    jobName:'',
-    jobRun:null,
-    jobEnd:null,
-    jobId:null
-}];
-exports.appointmentsModel = [{
-        name:'',
-        duration:60,
-        startTime:null,
-        endTime:null,
-        id:''
-    }];
+var cronJobs = [];
+exports.appointmentsModel = [];
 
 exports.addCal = function(appointmentsRaw){
     var appointments = timeHelper.grabAppointments(appointmentsRaw);
     if(!_.isEqual(this.appointmentsModel, appointments)){
         console.log('Change to the calendar');
-        this.appointmentsModel = appointments;
-        // console.log('APPT',this.appointmentsModel);
         clearOldJobs();
+        this.appointmentsModel = appointments;
+        console.log('appointments', this.appointmentsModel);
         if(this.appointmentsModel){
             console.log('job length ',this.appointmentsModel.length);
             for(var i =0;i<this.appointmentsModel.length;i++){
+                console.log(i);
                 cronJobs.push({
                     jobName:'job'+i,
                     jobId: this.appointmentsModel[i].id,
@@ -39,56 +27,21 @@ exports.addCal = function(appointmentsRaw){
                 });
             }
         }
-
-        console.log(cronJobs);
-        // console.log('appointmentsModel ',this.appointmentsModel);
-        // timeout(this.appointmentsModel);
     }
 };
-// timeout = function(apts){
-//     setTimeout(function(){
-//         // console.log('timeout', cronJobs);
-//         cronJobs.forEach(function(cronJob,index){
-//             console.log(cronJob.jobName);
-//             console.log(cronJob.jobId);
-//         });
-//     },5*1000);
-// };
-
 
 function clearOldJobs(){
     cronJobs.forEach(function(job){
-        console.log('jobs loop',job);
-
-        console.log('jobrun',job.jobRun);
-        if(job.jobRun && job.jobRun.Job){
-            console.log("FOUND");
+        if(job.jobRun){
+            job.jobRun.cancel();
         }
-        if(job.jobRun && job.jobRun.Job){
-            job.jobRun.Job.cancel();
-            console.log("canceling job");
-        }
-        if(job.jobEnd && job.jobEnd.Job){
-            job.jobEnd.Job.cancel();
+        if(job.jobEnd){
+            job.jobEnd.cancel();
         }
     });
 
-    cronJobs = [{
-        jobName:'',
-        jobRun:null,
-        jobEnd:null,
-        jobId:null
-    }];
+    cronJobs = [];
 }
-
-
-// j.cancel();
-
-
-setTimeout(function(){
-    console.log('stop job');
-    // task.destroy();
-},5*1000);
 
 jobStartTime = function(startTime){
     var job = null;
@@ -103,48 +56,14 @@ jobEndTime = function(endTime){
     console.log('job end time', endTime);
     var job = null;
     var cronEndTime = new Date(endTime.getTime() - minsBeforeFloat*60*1000);
-    console.log('cronEndTime',cronEndTime);
     return schedule.scheduleJob(cronEndTime, function(){
-        turnOn();
+        turnOff();
     });
 };
-
-
-
-var cronEndTimet = new Date();
-var ja = jobStartTime(cronEndTimet);
-
-
-test = function(){
-    var job;
-    job = schedule.scheduleJob(new Date(), function(){
-        turnOn();
-    });
-    return job;
-};
-var cronJobsTwo = [{
-    jobName:'sdf',
-    jobRun:jobStartTime(new Date()),
-    jobEnd:null,
-    jobId:null
-}];
-
-for(var i=0;i<cronJobsTwo.length;i++){
-    console.log('arr jobs',cronJobsTwo[i]);
-}
-
-cronJobsTwo.forEach(function(job){
-    console.log('alksdjf',job);
-    if(job.jobRun){
-        job.jobRun.cancel();
-        console.log('canceling');
-    }
-});
 
 turnOff = function(calendarObj){
     console.log(new Date()+' Turning off');
 };
-
 
 turnOn = function(calendarObj){
     console.log(new Date()+' Turning on ');
